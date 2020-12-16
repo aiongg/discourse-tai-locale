732ced003221895e4545899d90368bddac8c3fa9
Edward Greve <edward.greve@gmail.com>
2020-12-17 01:04:13 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#discourse-tai-locale
+
+Locale plugin for Tâi (Taioanese) locales.
+
+Includes both Lô-má-jī (`tai_Rom`) and Hàn-jī (`tai_Han`).
---
+++ plugin.rb
@@ -0,0 +1,23 @@
+# name: tai-locale
+# about: A plugin for Taiwanese language locale
+# version: 1.02
+
+register_locale(
+  "tai_Han",
+  name: "Taiwanese (Hanji)",
+  nativeName: "台語漢字",
+  plural: {
+    keys: [:other],
+    rule: lambda { |n| :other }
+  }
+)
+
+register_locale(
+  "tai_Rom",
+  name: "Taiwanese (Roman)",
+  nativeName: "Tâi-gí Lô-má-jī",
+  plural: {
+    keys: [:other],
+    rule: lambda { |n| :other }
+  }
+)
